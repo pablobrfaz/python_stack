2e3d9ba36fef0f4bcb9bdd20fdaaad174a36152f
pablobrfaz <pablojbravofaz@gmail.com>
2021-07-08 23:18:22 -0500

    EjercicioPalabraRandom

--- django/django_intro/palabale/random_word/urls.py
+++ django/django_intro/palabale/random_word/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('palabra_random', views.palabra_random),
+    path('reset',views.reset),
 ]
--- django/django_intro/palabale/random_word/views.py
+++ django/django_intro/palabale/random_word/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.crypto import get_random_string
+from django.http.response import HttpResponse
 
 def index(request):
+    return redirect("/palabra_random")
+
+def palabra_random(request):
     context = {
         "get_random_string": get_random_string(length=14),
         
     }
+    if 'count' not in request.session:
+        request.session['count'] = 1
+    else:
+        request.session['count'] += 1
+
     return render(request,'index.html', context)
 
 
+def reset(request):
+    request.session.clear()
+    return render(request,"index.html")
