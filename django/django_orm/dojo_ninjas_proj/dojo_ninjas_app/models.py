8e895de3ddd9ab7c0b91af8b3563ff4ed27ce2fb
pablobrfaz <pablojbravofaz@gmail.com>
2021-07-13 00:30:48 -0500

    ejerciciodojo y ninjas

---
+++ django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import TextField
+
+# Create your models here.
+class dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    desc = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ninjas(models.Model):
+    dojo = models.ForeignKey(dojos,related_name="ninjas",on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
